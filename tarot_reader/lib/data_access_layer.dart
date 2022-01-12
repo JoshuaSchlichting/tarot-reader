965f5226fbc721b8d422dd406ac5c6389e53c2e9
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2022-01-11 21:27:45 -0500

    add card data provider

--- tarot_reader/lib/data_access_layer.dart
+++ tarot_reader/lib/data_access_layer.dart
@@ -15,3 +15,12 @@ final imagePathProvider = FutureProvider<List<String>>((ref) async {
 
   return imagePaths;
 });
+
+final cardDataProvider = FutureProvider<Map<String, dynamic>>((ref) async {
+  final String jsonPayload =
+      await rootBundle.loadString('assets/images/decks/tarot-images.json');
+
+  Map<String, dynamic> data = json.decode(jsonPayload);
+
+  return data;
+});
