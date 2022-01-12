22954c1c9a1932a31efd69b0f4a4dcfdae13867c
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2022-01-12 00:20:50 -0500

    add model for card dataq

--- tarot_reader/lib/card_reading/tarot_data.dart
+++ tarot_reader/lib/card_reading/tarot_data.dart
@@ -1,15 +1,67 @@
+class TarotCardData {
+  late final String name;
+  late final int number;
+  late final String arcana;
+  late final String suit;
+  late final String img;
+  late final List<String> fortunteTelling;
+  late final List<String> keywords;
+  late final List<Map<String, List<String>>> meanings;
+  late final String archetype;
+  late final String hebrewAlphabet;
+  late final String numerology;
+  late final String elemental;
+  late final String mythicalSpiritual;
+  late final List<String> questionsToAsk;
+  TarotCardData(
+      {required this.name,
+      required this.number,
+      required this.arcana,
+      required this.suit,
+      required this.img,
+      required this.fortunteTelling,
+      required this.keywords,
+      required this.meanings,
+      required this.archetype,
+      required this.hebrewAlphabet,
+      required this.numerology,
+      required this.elemental,
+      required this.mythicalSpiritual,
+      required this.questionsToAsk});
+}
+
 class TarotDataProcessor {
-  late List<Map<String, dynamic>>? _data;
+  late List<TarotCardData> _data;
 
   TarotDataProcessor(Map<String, dynamic> data) {
-    _data = data["cards"];
+    // TODO: See what's up here, cards should be a List
+    List<Map> cardObjs = data["cards"] as List<Map>;
+    for (var element in cardObjs) {
+      _data.add(TarotCardData(
+        name: element["name"],
+        number: element["number"],
+        arcana: element["arcana"],
+        suit: element["suit"],
+        img: element["img"],
+        fortunteTelling: element["fortune_telling"],
+        keywords: element["keywords"],
+        meanings: element["meanings"],
+        archetype: element["Archetype"],
+        hebrewAlphabet: element["Hebrew Alphabet"],
+        numerology: element["Numerology"],
+        elemental: element["Elemental"],
+        mythicalSpiritual: element["Mythical/Spiritual"],
+        questionsToAsk: element["Questions To Ask"],
+      ));
+    }
+  }
+
+  List<TarotCardData> get availableImages {
+    return _data;
   }
 
   // Get's "card" in the context of "card" being the card map
-  Map<String, dynamic> getCardData(String name) {
-    if (_data == null) {
-      return {};
-    }
-    return _data!.firstWhere((card) => card['name'] == name);
+  TarotCardData getCardData(String name) {
+    return _data.firstWhere((card) => card.name == name);
   }
 }
